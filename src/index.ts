7ed60c5ea096589c047b95a8ca4cd2fb35e35e64
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-07-13 16:07:48 +0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+
+const typeDefs = `#graphql
+
+
+  
+  type Query {
+    
+  }
+`;
+
+
+
+const resolvers = {
+    Query: {
+    
+    },
+  };
+
+ 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
